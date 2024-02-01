935903aa784f11dc4bd151f3d2673a961910f298
supichaya1 <mailguahere@gmail.com>
2024-02-01 08:26:56 +0700

    fin week3

---
+++ Assets/Scripts/Factions/Faction/Faction.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Nation
+{
+    Neutal = 0,
+    Britain,
+    Pirates,
+    France,
+    Spain,
+    Portiguese,
+    Dutch
+}
+public class Faction : MonoBehaviour
+{
+    [SerializeField] private Nation nation;
+    public Nation Nation { get { return nation; } }
+    [Header("Resources")]
+    [SerializeField] private int food;
+    public int Food { get { return food; } set { food = value; } }
+    [SerializeField] private int wood;
+    public int Wood { get { return wood; } set { wood = value; } }
+    [SerializeField] private int gold;
+    public int Gold { get { return gold; } set { gold = value; } }
+    [SerializeField] private int stone;
+    public int Stone { get { return stone; } set { stone = value; } }
+    
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
