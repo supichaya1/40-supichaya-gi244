4554fe2fe217b0ff764942b8c00042e30f4f9f9b
supichaya1 <mailguahere@gmail.com>
2024-02-01 11:13:28 +0700

    VFX

--- Assets/Scripts/Unit/Unit.cs
+++ Assets/Scripts/Unit/Unit.cs
@@ -71,7 +71,13 @@ public class Unit : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        switch (state)
+        {
+            case UnitState.Move:
+                MoveUpdate();
+                break;
+        }
+ 
     }
     
     public void ToggleSelectionVisual(bool flag)
@@ -79,5 +85,34 @@ public class Unit : MonoBehaviour
         if (selectionVisual != null)
             selectionVisual.SetActive(flag);
     }
+    
+    public void SetState(UnitState toState)
+    {
+        state = toState;
 
+        if (state == UnitState.Idle)
+        {
+            navAgent.isStopped = true;
+            navAgent.ResetPath();
+        }
+    }
+    public void MoveToPosition(Vector3 dest)
+    {
+        if (navAgent != null)
+        {
+            navAgent.SetDestination(dest);
+            navAgent.isStopped = false;
+        }
+
+        SetState(UnitState.Move); 
+    }
+    
+    private void MoveUpdate()
+    {
+        float distance = Vector3.Distance(transform.position, navAgent.destination);
+
+        if (distance <= 1f)
+            SetState(UnitState.Idle);
+    }
+    
 }
