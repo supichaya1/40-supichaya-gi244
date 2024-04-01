75544e71b6f506fce7bcda6044d6ccc3d5839a7f
supichaya1 <mailguahere@gmail.com>
2024-04-01 20:55:30 +0700

    23.6 fin

--- Assets/Scripts/Command/UnitCommand.cs
+++ Assets/Scripts/Command/UnitCommand.cs
@@ -137,6 +137,23 @@ public class UnitCommand : MonoBehaviour
         // if it is an enemy's building
         if (building.Faction == GameManager.instance.EnemyFaction)
             UnitAttackEnemyBuilding(building, units);
+        else
+        {
+            if (building.CurHP < building.MaxHP)
+            {
+                HelpFixBuilding(hit.collider.gameObject,units);
+                StartCoroutine(Formula.BlinkSelection(building.SelectionVisual));
+            }
+        }
+    }
+    
+    private void HelpFixBuilding(GameObject target, List<Unit> units)
+    {
+        foreach (Unit u in units)
+        {
+            if (u.IsBuilder)
+                u.Builder.BuilderStartFixBuilding(target);
+        }
     }
     
     
