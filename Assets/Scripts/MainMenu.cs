0411b2cc93e38da4e5d3da9b852e6fa6f5f0d288
supichaya1 <mailguahere@gmail.com>
2024-04-24 08:39:10 +0700

    29.17

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private string newScene;
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(newScene);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
