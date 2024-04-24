701f9558c7f158546e83d4f0373d884339f8b75f
supichaya1 <mailguahere@gmail.com>
2024-04-24 12:23:10 +0700

    32.8

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -13,6 +13,7 @@ public class GameManager : MonoBehaviour
 
     //All factions in this game (2 factions for now)
     [SerializeField] private Faction[] factions;
+    public Faction[] Factions { get { return factions; } }
 
     public static GameManager instance;
 
